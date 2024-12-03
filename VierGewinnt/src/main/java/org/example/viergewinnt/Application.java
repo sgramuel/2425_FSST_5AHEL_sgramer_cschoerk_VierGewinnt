280bf60fff9f17d8321389b5f226d2debe7112fb
CSchoerki <cschoerk@htl-steyr.ac.at>
2024-12-03 14:34:51 +0100

    Anpassen des Codes auf JavaFX

--- VierGewinnt/src/main/java/org/example/viergewinnt/Application.java
+++ VierGewinnt/src/main/java/org/example/viergewinnt/Application.java
@@ -1,11 +1,7 @@
 package org.example.viergewinnt;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
